600eb2e307a4023f572dab16099f09a0994a019d
adensahwaludin <sekha1211@gmail.com>
2026-05-14 01:02:20 +0700

    Fix: Resolve ENOENT errors in Admin API endpoints by correcting import paths

--- server/api/users/[id]/reset-password.post.ts
+++ server/api/users/[id]/reset-password.post.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcryptjs'
-import { prisma } from '~/server/utils/prisma'
+import { prisma } from '../../../utils/prisma'
 
 export default defineEventHandler(async (event) => {
   const userId = await requireAuth(event)
